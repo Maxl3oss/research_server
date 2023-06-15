993c61eff12a4ce80f404918255a2f0d5a31d806
PondPhuket <narongrid.dev@gmail.com>
2023-06-16 06:46:20 +0700

    vercel => change1.2

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -1,31 +1,31 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-async function Create() {
-  // const newUser = await prisma.user.create({
-  //   data: {
-  //     fname: "Narongrid",
-  //     lname: "Naorkham",
-  //     email: "narongrid.dev@gmail.com",
-  //     password: "asdf",
-  //     role_id: 1,
-  //     status: 1,
-  //   },
-  // });
+// async function Create() {
+//   // const newUser = await prisma.user.create({
+//   //   data: {
+//   //     fname: "Narongrid",
+//   //     lname: "Naorkham",
+//   //     email: "narongrid.dev@gmail.com",
+//   //     password: "asdf",
+//   //     role_id: 1,
+//   //     status: 1,
+//   //   },
+//   // });
 
-  const GetUser = await prisma.user.findUnique({
-    where: {
-      id: "cli1w14it0000zvx59dmmlc2b",
-    },
-    select: {
-      fname: true,
-      lname: true,
-      email: true,
-    },
-  });
+//   const GetUser = await prisma.user.findUnique({
+//     where: {
+//       id: "cli1w14it0000zvx59dmmlc2b",
+//     },
+//     select: {
+//       fname: true,
+//       lname: true,
+//       email: true,
+//     },
+//   });
 
-  return GetUser;
-}
+//   return GetUser;
+// }
 
-Create().then((res) => console.log(res));
+// Create().then((res) => console.log(res));
