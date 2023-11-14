fe2477fabb31036bd3f7ad324f59949f00272f58
PondPhuket <narongrid.dev@gmail.com>
2023-11-14 20:41:11 +0700

    user => crud

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,35 +1,22 @@
 import { Request, Response } from "express";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, User } from "@prisma/client";
 import { sendErrorResponse, sendSuccessResponse, createPagination } from "../interface/response.interface";
+import { uploadFilesHelper } from "../utils/helper.util";
+import bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
-async function GetUser(req: Request, res: Response) {
+export async function ChangeProfile(req: Request, res: Response) {
   try {
-    const { page = 1, pageSize = 10 } = req.body;
-    const skip = (page - 1) * pageSize;
-    const GetUser = await prisma.user.findMany({
-      skip,
-      take: pageSize,
-      where: {
-        status: 1,
-      },
-      select: {
-        id: true,
-        first_name: true,
-        last_name: true,
-        email: true,
-      },
-    });
-    const total = await prisma.user.count({
-      where: {
-        status: 1,
-      },
+    const { id } = req.params;
+    const { profile_url } = await uploadFilesHelper(req.files, res);
+    const query = await prisma.user.update({
+      where: { id: id },
+      data: { ...((profile_url && profile_url !== "") && { profile: profile_url }) }
     });
 
-    if (!GetUser) sendErrorResponse(res, "User not found.", 404);
-
-    sendSuccessResponse(res, "success", GetUser, createPagination(page, pageSize, total));
+    if (!query) sendErrorResponse(res, "Change profile fail.", 404);
+    sendSuccessResponse(res, "success", undefined);
 
   } catch (err) {
     console.error(err);
@@ -39,4 +26,137 @@ async function GetUser(req: Request, res: Response) {
   }
 }
 
-export { GetUser as GetProfile }
+export async function GetProfile(req: Request, res: Response) {
+  try {
+    const { id } = req.params;
+    const query = await prisma.user.findFirst({
+      where: { id: id },
+      select: {
+        id: true,
+        email: true,
+        password: true,
+        profile: true,
+        first_name: true,
+        last_name: true,
+        status: true,
+        role_id: true,
+        prefix: true,
+      },
+    });
+
+    if (!query) sendErrorResponse(res, "No user profile.", 404);
+    sendSuccessResponse(res, "success", query);
+
+  } catch (err) {
+    console.error(err);
+    sendErrorResponse(res, "Internal server error.");
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+// admin
+export async function GetUsersAll(req: Request, res: Response) {
+  try {
+    const query = await prisma.user.findMany({
+      where: { status: { not: 0 } },
+      select: {
+        id: true,
+        prefix: true,
+        first_name: true,
+        last_name: true,
+        status: true,
+      }
+    });
+
+    if (!query) sendErrorResponse(res, "User not found.", 404);
+    sendSuccessResponse(res, "success", undefined);
+
+  } catch (err) {
+    console.error(err);
+    sendErrorResponse(res, "Internal server error.");
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+export async function DeleteUsersById(req: Request, res: Response) {
+  try {
+    const { id } = req.params;
+    const query = await prisma.user.update({ where: { id: id }, data: { status: 0 } });
+
+    if (!query) sendErrorResponse(res, "User not found.", 404);
+    sendSuccessResponse(res, "success", undefined);
+
+  } catch (err) {
+    console.error(err);
+    sendErrorResponse(res, "Internal server error.");
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+export async function GetUserById(req: Request, res: Response) {
+  try {
+    const { id } = req.params;
+    const query = await prisma.user.findFirst({ where: { id: id } });
+
+    if (!query) sendErrorResponse(res, "User not found.", 404);
+    sendSuccessResponse(res, "success", undefined);
+
+  } catch (err) {
+    console.error(err);
+    sendErrorResponse(res, "Internal server error.");
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+export async function verifyUserById(req: Request, res: Response) {
+  try {
+    const { id } = req.params;
+    const statusPrev = await prisma.user.findFirst({ where: { id: id } });
+    const query = await prisma.user.update({ where: { id: id }, data: { status: statusPrev?.status === 2 ? 1 : 2 } });
+
+    if (!query) sendErrorResponse(res, "User not found.", 404);
+    sendSuccessResponse(res, "success", undefined);
+
+  } catch (err) {
+    console.error(err);
+    sendErrorResponse(res, "Internal server error.");
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+export async function Update(req: Request, res: Response) {
+  try {
+    const { id } = req.params;
+    const data: User = req.body;
+    const { profile_url } = await uploadFilesHelper(req.files, res);
+    const hashPass = await bcrypt.hash(data.password, 9);
+
+    const query = await prisma.user.update({
+      where: {
+        id: id,
+      },
+      data: {
+        prefix: data.prefix,
+        first_name: data.first_name,
+        last_name: data.last_name,
+        email: data.email,
+        ...(hashPass && { password: hashPass }),
+        ...((profile_url && profile_url !== "") && { profile: profile_url }),
+      }
+    });
+
+    if (!query) sendErrorResponse(res, "Update user fail.", 404);
+    sendSuccessResponse(res, "success", undefined);
+
+  } catch (err) {
+    console.error(err);
+    sendErrorResponse(res, "Internal server error.");
+  } finally {
+    await prisma.$disconnect();
+  }
+}
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,8 +1,20 @@
 import express from "express";
 import userController = require("../controllers/user.controller");
 const router = express.Router();
+import { authenticateJWT, authorizeAdmin } from '../middleware/authMiddleware';
+import iMulter from "../utils/multer.util";
 require("dotenv").config();
 
-router.get("/fetch-user", userController.GetProfile);
+router.put("/change-profile/:id",
+  iMulter.uploads.fields([{ name: 'profile', maxCount: 1 }]),
+  authenticateJWT, userController.ChangeProfile);
+
+router.get("/get-profile/:id", authenticateJWT, userController.GetProfile);
+
+// admin
+router.get("/managements/get-all", authenticateJWT, authorizeAdmin, userController.GetUsersAll);
+router.get("/managements/get/:id", authenticateJWT, authorizeAdmin, userController.GetUserById);
+router.delete("/managements/delete-user/:id", authenticateJWT, authorizeAdmin, userController.DeleteUsersById);
+router.put("/managements/verify/:id", authenticateJWT, authorizeAdmin, userController.verifyUserById);
 
 export default router;
